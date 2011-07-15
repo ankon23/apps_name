07bc2c53c5982b5987a85028ae07d55f9ab39702
Ants <ants@ants-VirtualBox.(none)>
2011-07-15 16:29:49 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppsName::Application.config.secret_token = 'f8f11a32392034a658b7625de7d46bca894d631909e942f1b1109615917e5317acf34d438c414f38f5988a9436dfa893076d31a9787f927ef69007b033b7e83f'
